b731b21503dfec4a9ad51731da27c5b84fe9cfd5
pricillia <pricillia.edou@gmail.com>
2024-12-02 16:52:55 +0100

    modification du header en fonction de l'état connexion ou pas

--- front-end_gamepad/src/composants/Header.jsx
+++ front-end_gamepad/src/composants/Header.jsx
@@ -10,32 +10,27 @@ const Header = ({ token, setUser }) => {
         <img src={Logo} alt='logo Gamepad' className='w-7' />
         <p>Gamepad</p>
       </div>
-      {token ? (
+      {!token ? (
         <div className='flex justify-center items-center'>
-          <button
-            onClick={setUser(null)}
-            className='flex bg-[#ff4655] justify-center items-center py-1 px-2 rounded pointer'
-          >
-            <p>Se déconnecter</p>
-          </button>
-        </div>
-      ) : (
-        <div className='flex justify-center items-center'>
-          <button
-            onClick={() => {
-              navigate("/signup");
-            }}
-            className='flex bg-[#ff4655] justify-center items-center py-1 px-2 rounded pointer'
-          >
-            <p>Se connecter</p>
-          </button>
           <button
             onClick={() => {
               navigate("/login");
             }}
-            className='header-button button-login-signup'
+            className='flex bg-[#ff4655] justify-center items-center py-1 px-2 rounded pointer'
           >
-            Se connecter
+            Login
+          </button>
+        </div>
+      ) : (
+        <div className='flex justify-center items-center'>
+          <button className='flex  justify-center items-center py-1 px-2 rounded pointer'>
+            <p>My collection</p>
+          </button>
+          <button
+            onClick={() => setUser(null)}
+            className='flex bg-[#ff4655] justify-center items-center py-1 px-2 rounded pointer'
+          >
+            <p>Disconnect</p>
           </button>
         </div>
       )}
